ac6e2e29a63356c9744ad83130d3efa1ad585cb6
MortCord <ratynsky98@gmail.com>
2022-12-04 22:32:03 +0200

    Alpha version

---
+++ js/index.js
@@ -0,0 +1,111 @@
+function change() {
+    const el = document.getElementsByClassName("infoStud");
+    for(var x = 0; x < el.length;x++){
+        el[x].style.visibility="visible";
+    }
+}
+
+function changeBack() {
+    const el = document.getElementsByClassName("infoStud");
+    for(var x = 0; x < el.length;x++){
+        el[x].style.visibility="hidden";
+    }
+}
+
+function change2() {
+    const el = document.getElementsByClassName("infoStud2");
+    for(var x = 0; x < el.length;x++){
+        el[x].style.visibility="visible";
+    }
+}
+
+function changeBack2() {
+    const el = document.getElementsByClassName("infoStud2");
+    for(var x = 0; x < el.length;x++){
+        el[x].style.visibility="hidden";
+    }
+}
+
+function change3() {
+    const el = document.getElementsByClassName("infoStud3");
+    for(var x = 0; x < el.length;x++){
+        el[x].style.visibility="visible";
+    }
+}
+
+function changeBack3() {
+    const el = document.getElementsByClassName("infoStud3");
+    for(var x = 0; x < el.length;x++){
+        el[x].style.visibility="hidden";
+    }
+}
+
+function change4() {
+    const el = document.getElementsByClassName("infoStud4");
+    for(var x = 0; x < el.length;x++){
+        el[x].style.visibility="visible";
+    }
+}
+
+function changeBack4() {
+    const el = document.getElementsByClassName("infoStud4");
+    for(var x = 0; x < el.length;x++){
+        el[x].style.visibility="hidden";
+    }
+}
+
+function change5() {
+    const el = document.getElementsByClassName("infoStud5");
+    for(var x = 0; x < el.length;x++){
+        el[x].style.visibility="visible";
+    }
+}
+
+function changeBack5() {
+    const el = document.getElementsByClassName("infoStud5");
+    for(var x = 0; x < el.length;x++){
+        el[x].style.visibility="hidden";
+    }
+}
+
+function change6() {
+    const el = document.getElementsByClassName("infoStud6");
+    for(var x = 0; x < el.length;x++){
+        el[x].style.visibility="visible";
+    }
+}
+
+function changeBack6() {
+    const el = document.getElementsByClassName("infoStud6");
+    for(var x = 0; x < el.length;x++){
+        el[x].style.visibility="hidden";
+    }
+}
+
+function change7() {
+    const el = document.getElementsByClassName("infoStud7");
+    for(var x = 0; x < el.length;x++){
+        el[x].style.visibility="visible";
+    }
+}
+
+function changeBack7() {
+    const el = document.getElementsByClassName("infoStud7");
+    for(var x = 0; x < el.length;x++){
+        el[x].style.visibility="hidden";
+    }
+}
+
+function change8() {
+    const el = document.getElementsByClassName("infoStud8");
+    for(var x = 0; x < el.length;x++){
+        el[x].style.visibility="visible";
+    }
+}
+
+function changeBack8() {
+    const el = document.getElementsByClassName("infoStud8");
+    for(var x = 0; x < el.length;x++){
+        el[x].style.visibility="hidden";
+    }
+}
