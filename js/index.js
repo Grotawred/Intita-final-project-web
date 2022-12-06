f384233597190eba0a5efc2d313d155ba3b02886
MortCord <ratynsky98@gmail.com>
2022-12-06 19:48:30 +0200

    With dark mode

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+var ifDark = false;
+
 function change() {
     const el = document.getElementsByClassName("infoStud");
     for(var x = 0; x < el.length;x++){
@@ -107,5 +109,35 @@ function changeBack8() {
     const el = document.getElementsByClassName("infoStud8");
     for(var x = 0; x < el.length;x++){
         el[x].style.visibility="hidden";
+    }
+}
+
+function DarkTheme(){
+    if(ifDark == false){
+        ifDark = true
+        document.getElementById('lamp').src="./img/turn-on.svg";
+        document.getElementById('header').style.backgroundColor = "#1B1F2F";
+        document.getElementById('mainHeader').style.color = "white";
+        document.getElementById('lang').style.color = "white";
+        document.getElementById('body').style.backgroundColor = "#1B1F2F";
+        document.getElementById('body').style.color = "white";
+        document.getElementById('moreBut').style.backgroundColor = "#C9D5FF";
+        document.getElementById('moreBut').style.color = "black";
+        document.getElementById('slider-s').style.color = "white";
+        document.getElementById('slider-l').style.color = "white";
+        document.getElementById('footer').style.color = "white !important";
+    }else{
+        ifDark = false
+        document.getElementById('lamp').src="./img/turn-on2.svg";
+        document.getElementById('header').style.backgroundColor = "#C9D5FF";
+        document.getElementById('mainHeader').style.color = "black";
+        document.getElementById('lang').style.color = "black";
+        document.getElementById('body').style.backgroundColor = "#C9D5FF"; 
+        document.getElementById('body').style.color = "black";
+        document.getElementById('moreBut').style.backgroundColor = "#1B1F2F";
+        document.getElementById('moreBut').style.color = "black";
+        document.getElementById('slider-s').style.color = "black";
+        document.getElementById('slider-l').style.color = "black";
+        document.getElementById('footer').style.color = "black !important";
     }
 }
