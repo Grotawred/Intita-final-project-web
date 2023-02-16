adf996a80975b77b2a7a9178d2d5253924107339
MortCord <ratynsky98@gmail.com>
2023-02-16 18:45:55 +0200

    v3.1

---
+++ ITA_Project v3/js/student.js
@@ -0,0 +1,9 @@
+function showInfo() {
+    const el = document.getElementById("hoverjs-active");
+    el.style.visibility="visible";
+}
+
+function hideInfo(){
+    const el = document.getElementById("hoverjs-active");
+    el.style.visibility="hidden";
+}
