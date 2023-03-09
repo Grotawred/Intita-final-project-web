87814b6a094672dd4160cda643f5184612489a12
MortCord <ratynsky98@gmail.com>
2023-03-09 18:28:28 +0200

    v3.2

--- ITA_Project v3/js/index.js
+++ ITA_Project v3/js/index.js
@@ -1,33 +1,53 @@
+onload = function(){
+    checkMobile();
+}
+
+
+
+function checkMobile(){
+    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+        // true for mobile device
+        console.log("mobile device");
+        window.location.href = "index-mobile.html";
+        return true;
+      }else{
+        // false for not mobile device
+        console.log("not mobile device");
+        return false;
+      }
+}
+
+
 function changeInfo(name){
     const elText = document.getElementById('studentName');
     const elImg = document.getElementById('studentImg');
-    const credoText = document.getElementById('credo')
+    const credoText = document.getElementById('credo');
     elText.innerHTML=name;
     switch(name){
         case 'Володимир Ратинський':
-            elImg.src="./img/Vova_big.png"
+            elImg.src="./img/Vova_big.png";
             elImg.style.width='350px';
-            credoText.innerHTML="wenomechainsama tumajarbisaun Wifenlooof eselifterbraun Anweculbetugtbaby Aslonskysrblu Yuaksoinocenow Buchyulaidsosun wenomechainsama"
+            credoText.innerHTML="wenomechainsama tumajarbisaun Wifenlooof eselifterbraun Anweculbetugtbaby Aslonskysrblu Yuaksoinocenow Buchyulaidsosun wenomechainsama";
             break;
         case 'Механічева Олександра':
-            elImg.src="./img/Slider_student1.1.png"
+            elImg.src="./img/Slider_student1.1.png";
             elImg.style.width='400px';
-            credoText.innerHTML="is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an"
+            credoText.innerHTML="is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an";
             break;
         case 'Гурський Даніл':
-            elImg.src="./img/Danil_big.png"
+            elImg.src="./img/Danil_big.png";
             elImg.style.width='462px';
-            credoText.innerHTML="is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an"
+            credoText.innerHTML="is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an";
             break;
         case 'Костя Гавриш':
-            elImg.src="./img/Kostya_big.png"
+            elImg.src="./img/Kostya_big.png";
             elImg.style.width='462px';
-            credoText.innerHTML="is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an"
+            credoText.innerHTML="is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an";
             break;
         case 'Власко Григорій':
-            elImg.src="./img/Cringe.png"
+            elImg.src="./img/Cringe.png";
             elImg.style.width='400px';
-            credoText.innerHTML="is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an"
+            credoText.innerHTML="is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an";
             break;
     }
 }
@@ -66,4 +86,29 @@ function changeInfoGraduate(name){
             break;
     }
 }
+
+function hoverMoreArrowStudent(el,isTrue){
+    if(isTrue){
+        el.src = "./img/arrow-short-left.png";
+    }else{
+        el.src = "./img/moreArrowHover.png";
+    }
+}
+
+function hoverMoreArrowGraduate(el,isTrue){
+    if(isTrue){
+        el.src="./img/arrow-short-right3.png";
+    }else{
+        el.src="./img/arrow-short-right.png";
+    }
+}
+
+function hoverMoreArrowLocation(el,isTrue){
+    if(isTrue){
+        el.src="./img/arrow-location-hover.png";
+    }else{
+        el.src="./img/location-arrow.png";
+    }
+}
+
 
