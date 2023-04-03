568257217a00f8f79a35e7aa372072fa9c9d25b2
Mortcord <ratynsky98@gmail.com>
2023-04-03 17:41:40 +0300

    V3.4 - hacker's and graduate's pages have been added

---
+++ ITA_Project v3/js/graduate.js
@@ -0,0 +1,26 @@
+onload = function(){
+    checkMobile();
+}
+
+
+
+function checkMobile(){
+    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+        // true for mobile device
+        console.log("mobile device");
+        window.location.href = ""//Link to mobile version;
+        return true;
+      }else{
+        // false for not mobile device
+        console.log("not mobile device");
+        return false;
+      }
+}
+
+function hoverMoreArrowLocation(el,isTrue){
+    if(isTrue){
+        el.src="./img/arrow-location-hover.png";
+    }else{
+        el.src="./img/location-arrow.png";
+    }
+}
